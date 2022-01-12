9ca6986a8057605f7d82894bb9bb785cbc17ce8c
tinoschroeter <tino.schroeter@gmail.com>
2022-01-12 18:58:15 +0100

    add joke machine

--- README.md
+++ README.md
@@ -27,7 +27,8 @@ const tino = {
       "Haproxy",
     ],
     cloud: ["AWS", "GCP", "Firebase"],
-    IoT: ["Arduino", "Raspberry Pi", "ESP8266", "3D Printing"]
+    IoT: ["Arduino", "Raspberry Pi", "ESP8266", "3D Printing"],
+    joke: "I've got a really good UDP joke to tell you but I don’t know if you'll get it."
   },
 };
 ```
---
+++ index.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+("use srict");
+
+const axios = require("axios").default;
+const api =
+  "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&format=txt&type=single";
+
+axios
+  .get(api)
+  .then((response) => {
+    const joke = response.data.replace(/(?:\r\n|\r|\n)/g, " ").replace(/"/g, "");
+
+    fs.readFile("./README.md", "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+
+      const updateFile = data.replace(/^.*joke:.*$/gm, `    joke: "${joke}"`);
+
+      fs.writeFile("./README.md", updateFile, (err) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+      });
+    });
+  })
+  .catch((error) => {
+    console.error(error);
+  });
