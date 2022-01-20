63bba66df186c8f323d3fd30ec665398d899650a
tinoschroeter <tino.schroeter@gmail.com>
2022-01-20 20:02:24 +0100

    add debug line

--- weather.js
+++ weather.js
@@ -7,6 +7,7 @@ weather.find({ search: "Hamburg", degreeType: "C" }, (err, result) => {
 
   const data = result[0].current;
   const date = new Date();
+  console.log(data)
 
   let picker = 0;
   const icon = ["", "☁️", "❄️", "☀️", "🌧️", "🌩️", "🌜"];
